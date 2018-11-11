821a8cb578d73f49bddb47a40d9b8499065959bc
aruyamam <aruyamam@gmail.com>
2018-11-11 15:16:20 +0900

    Converting decimal numbers to binary

---
+++ examples/04_Stack/03-DecimalToBinary.js
@@ -0,0 +1,11 @@
+const { decimalToBinary } = PacktDataStructuresAlgorithms;
+const { baseConverter } = PacktDataStructuresAlgorithms;
+
+console.log(decimalToBinary(233));
+console.log(decimalToBinary(10));
+console.log(decimalToBinary(1000));
+
+console.log(baseConverter(100345, 2));
+console.log(baseConverter(100345, 8));
+console.log(baseConverter(100345, 16));
+console.log(baseConverter(100345, 35));
--- src/js/index.js
+++
@@ -1,2 +0,0 @@
-export { default as StackArray } from './data-structures/stack-array';
-export { default as Stack } from './data-structures/stack-es6';
