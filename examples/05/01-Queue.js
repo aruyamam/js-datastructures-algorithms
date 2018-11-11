11bd1a1e5a9078d62dce0aa847e2a45593b0bc8e
aruyamam <aruyamam@gmail.com>
2018-11-11 15:53:13 +0900

    The queue data structure

---
+++ examples/05/01-Queue.js
@@ -0,0 +1,13 @@
+const { Queue } = PacktDataStructuresAlgorithms;
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue('John');
+queue.enqueue('Jack');
+console.log(queue.toString());
+queue.enqueue('Camila');
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.dequeue();
+queue.dequeue();
+console.log(queue.toString());
