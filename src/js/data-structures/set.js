6dc8592a84201593d801cbbe599c39e92f2a694f
aruyamam <aruyamam@gmail.com>
2018-11-19 16:15:08 +0900

    Set operations

--- src/js/data-structures/set.js
+++ src/js/data-structures/set.js
@@ -37,6 +37,62 @@ export default class Set {
       return values;
    }
 
+   union(otherSet) {
+      const unionSet = new Set();
+      this.values().forEach(value => unionSet.add(value));
+      otherSet.values().forEach(value => unionSet.add(value));
+      return unionSet;
+   }
+
+   intersection(otherSet) {
+      const intersectionSet = new Set();
+      const values = this.values();
+      const otherValues = otherSet.values();
+      let biggerSet = values;
+      let smallerSet = otherValues;
+
+      if (otherValues.length - values.length > 0) {
+         biggerSet = otherValues;
+         smallerSet = values;
+      }
+      smallerSet.forEach((value) => {
+         if (biggerSet.includes(value)) {
+            intersectionSet.add(value);
+         }
+      });
+
+      return intersectionSet;
+   }
+
+   difference(otherSet) {
+      const differenceSet = new Set();
+      this.values().forEach((value) => {
+         if (!otherSet.has(value)) {
+            differenceSet.add(value);
+         }
+      });
+      return differenceSet;
+   }
+
+   isSubsetOf(otherSet) {
+      if (this.size() > otherSet.size()) {
+         return false;
+      }
+      let isSubset = true;
+      this.values().every((value) => {
+         if (!otherSet.has(value)) {
+            isSubset = false;
+            return false;
+         }
+         return true;
+      });
+      return isSubset;
+   }
+
+   isEmpty() {
+      return this.size() === 0;
+   }
+
    size() {
       return Object.keys(this.items).length;
    }
@@ -53,5 +109,17 @@ export default class Set {
 
    clear() {
       this.itmes = {};
+   }
+
+   toString() {
+      if (this.isEmpty()) {
+         return '';
+      }
+      const values = this.values();
+      let objString = `${values[0]}`;
+      for (let i = 1; i < values.length; i++) {
+         objString = `${objString},${values[i].toString()}`;
+      }
+      return objString;
    }
 }
---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+export { default as StackArray } from './data-structures/stack-array';
+export { default as Stack } from './data-structures/stack-es6';
+export { decimalToBinary } from './others/base-converter';
+export { baseConverter } from './others/base-converter';
+
+// chapter05
+export { default as Queue } from './data-structures/queue';
+export { default as Deque } from './data-structures/deque';
+export { hotPotato } from './others/hot-potato';
+export { palindromeChecker } from './others/palindrome-checker';
+
+// chapter 06
+export { default as LinkedList } from './data-structures/linked-list';
+
+// chapter 07
+export { default as Set } from './data-structures/set';
+
+// chapter 09
+export { fibonacci, fibonacciMemoization } from './others/fibonacci';
